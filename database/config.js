4c26e5f8edeae703375ffb60c82a310ddb97d928
Alejandro Miranda <ale.miranda.barrera@gmail.com>
2023-03-23 23:36:59 -0500

    mogoose connection with config object

--- database/config.js
+++ database/config.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const dbConnection = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,                       
+            useUnifiedTopology: true,
+            useFindAndModify: false,                 
+            useCreateIndex: true              // to handle collection.ensureIndex is deprecated
+
+            });
         console.log('base de datos online');
 
     } catch (error) {
